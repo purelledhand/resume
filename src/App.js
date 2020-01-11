1f5e5f4f5566818d229442c0fc9b9bcad8b393ad
SoHye Choi <wergreat10@naver.com>
2020-01-12 06:56:10 +0900

    Publish Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import ContactCard from './components/ContactCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left">
+        <ContactCard></ContactCard>
+      </div>
+      <div className="right">
+        <div className="intro">
+          영감을 주는 인터페이스와 인터랙션을 창조하고자 합니다.
+          16살 때 포토샵과 HTML으로 개인 홈페이지를 만든 것을 시작으로 웹퍼블리싱과 디자인을 시작하게 되었습니다.
+          고등학생이 되어 Vanilla JS를 사용하며 본격적으로 웹 프론트엔드를 시작하였으며, 
+          현재는 고도화된 FE 생태계와 JS 런타임 엔진, Devops를 공부하고 있습니다.
+        </div>
+        <h2>
+          Curriculum Vitae
+        </h2>
+      </div>
     </div>
   );
 }
---
+++ src/components/ContactCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ContactCard.css';
+
+export function ContactCard() {
+    return (
+        <div className="contact">
+            <div>
+                <div className="contact-title">
+                    최소혜, Choi Sohye
+                </div>
+                <div className="contact-subtitle">
+                    Web Front-End, Devops.
+                </div>
+            </div>
+            <div>
+                <div className="contact-desc">
+                    Git | <a href="https:/github.com/purelledhand">https:/github.com/purelledhand</a>
+                </div>
+                <div className="contact-desc">
+                    Email | purelledhand@gmail.com
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ContactCard;
